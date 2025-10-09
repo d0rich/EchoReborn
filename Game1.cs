3c6c621b2c4b43e200f2246d3cab7f4b2f79e42f
Nikolai Dorofeev <contact@d0rich.me>
2025-10-09 16:35:55 +0200

    Solution and project in the same directory

--- Game1.cs
+++ Game1.cs
@@ -32,7 +32,8 @@ public class Game1 : Game
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+            Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
         // TODO: Add your update logic here
